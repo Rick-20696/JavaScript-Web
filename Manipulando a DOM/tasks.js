90e3c1291c2e49ceb740eb909a48f73ce05e4c51
Ricardo <ricarosousa83@gmail.com>
2020-09-29 22:32:10 -0300

    Manipulação da DOM

--- Manipulando a DOM/tasks.js
+++ Manipulando a DOM/tasks.js
@@ -1,29 +1,32 @@
+//Primeiramente tenho que ter o controle da minha Lista, Caixa de Texto e do Botão
 const list = document.getElementById("list");
 const input = document.getElementById("txt");
 const add = document.getElementById("btn");
 
-let tasks = [
-    "Make a coffee",
-    "Read a book",
-    "Make CRM"
-];
+//Aqui eu teria um array com as minhas tarefas, mas estou utilizando o LocalStorage para armazernar. A vantagem é que mesmo atualizando a página, eu tenho meus valores
+//Inicialmente ele vem como um JSON, então eu preciso converter para array e como eu posso não ter nada, preciso de um array vazio para não dar erros
+let tasks = JSON.parse(localStorage.getItem("ListTasks")) || [];
 
 const removeTask = function (pos) {
+    //splice remove o elemento do array
     tasks.splice(pos, 1);
 
     render();
-}
+    saveTask();
+}   
 
 const addTask = function () {
     tasks.push(input.value);
     
     input.value = "";
     render();
+    saveTask();
 }
 
 const render = function () {
     list.innerHTML = "";
 
+    //Aqui eu preciso criar as "<li>" e setar os textos e atributos e depois adicionar como filhos de outros elementos
     for (task of tasks){
         let li = document.createElement("li");
         let link = document.createElement("a");
@@ -31,7 +34,8 @@ const render = function () {
         let textLi = document.createTextNode(task);
         let textLink = document.createTextNode("Exclude"); 
         
-        var position = task.indexOf();
+        //Aqui eu uso o link e o índice de acordo com array para fazer a exclusão, depois associo o "onclick" ao "removeTask" passando como parametro a posição de índice 
+        var position = tasks.indexOf(task);
 
         link.setAttribute("onclick", "removeTask(" + position + ")")
         link.setAttribute("href", "#");
@@ -41,6 +45,12 @@ const render = function () {
         li.appendChild(link);
         list.appendChild(li);
     }
+}
+
+//LocalStorage é um armazenador de dados do tipo chave-valor. É mais utilizado em coisas simples que utilizam banco de dados
+const saveTask = function () {
+    //A variável não tem capacidade ler estruturas complexas, então precisa converter para JSON quer é uma estrutura ideal
+    localStorage.setItem('ListTasks', JSON.stringify(tasks));
 }
 
 render()
