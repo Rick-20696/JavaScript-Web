9493f87bff5c130e5c743e00491a5902fb08e68c
Ricardo <ricarosousa83@gmail.com>
2020-10-05 18:47:39 -0300

    Atualizando o server.js e a pasta src

--- Node/Node-API/server.js
+++ Node/Node-API/server.js
@@ -1,32 +1,27 @@
 //importando bibliotecas/frames/microserviços...
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //Iniciando o app
 const app = express();
 
+//Com esse método eu permito enviar informações no formato JSON para a base!
+app.use(express.json());
+
+//Com o CORS eu permito acesso de outros dominíos, não só do localhost ou insonmia
+app.use(cors());
 //Conectando com o mongo
 mongoose.connect(
     'mongodb://localhost:27017/nodeapi',
     { useNewUrlParser: true, useUnifiedTopology: true}
 );
 
-//Temos que exigir que o model esteja registrado!
+//Temos que exigir que todos os models esteja registrados!
 requireDir('./src/models');
 
-//Fazendo o primeiro teste com
-const Product = mongoose.model('Product');
-
-//Fazendo a primeira rota
-app.get('/', (request, response) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'https://github.com/facebook/react-native',
-    });
-
-    return response.send('Eae garai');
-});
-
+//As rotas agora tem lugar próprio './src/routes.js'
+//No lugar da minha rota, eu coloco o "use()" que recebe todo tipo de requisição e manda para o arquivo das rotas.
+app.use('/api', require('./src/routes'));
 app.listen(3001);
