5f900522dfb56b5108de3fa581703f7aad356918
Ricardo <ricarosousa83@gmail.com>
2020-10-07 18:15:35 -0300

    Criando uma classe "genérica" para fazer herança

--- ES6 + a fundo/bundle.js
+++ ES6 + a fundo/bundle.js
@@ -1,5 +1,21 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -11,40 +27,38 @@ var List = /*#__PURE__*/function () {
   function List() {
     _classCallCheck(this, List);
 
-    this.datas = [];
-  }
+    this.array = [];
+  } //Como meu contexto é de uma classe, eu preciso do this para acessar os atributos
+
 
   _createClass(List, [{
     key: "add",
-    value: function add(data) {
-      this.datas.push(data);
+    value: function add(element) {
+      this.array.push(element);
     }
   }]);
 
   return List;
-}();
+}(); //Isso é uma herança
 
-var TaskList = /*#__PURE__*/function () {
+
+var TaskList = /*#__PURE__*/function (_List) {
+  _inherits(TaskList, _List);
+
+  var _super = _createSuper(TaskList);
+
   function TaskList() {
     _classCallCheck(this, TaskList);
 
-    this.tasks = [];
-  } //Como meu contexto é de uma classe, eu preciso do this para acessar os atributos
-
-
-  _createClass(TaskList, [{
-    key: "addTask",
-    value: function addTask() {
-      this.tasks.push('New task');
-      console.log(this.tasks);
-    }
-  }]);
+    return _super.apply(this, arguments);
+  }
 
   return TaskList;
-}();
+}(List);
 
 var myList = new TaskList();
 
 document.getElementById('newTask').onclick = function () {
-  myList.addTask();
+  //Funciona do mesmo jeito
+  myList.add();
 };
--- ES6 + a fundo/main.js
+++ ES6 + a fundo/main.js
@@ -1,29 +1,23 @@
 //Códigos antes do ES6 será convertido para 
+
 class List {
     constructor() {
-        this.datas = [];
-    }
-
-    add(data) {
-        this.datas.push(data);
-    }
-}
-
-
-class TaskList {
-    
-    constructor(){
-        this.tasks = [];
+        this.array = [];
     }
 
     //Como meu contexto é de uma classe, eu preciso do this para acessar os atributos
-    addTask() {
-        this.tasks.push('New task'); 
-        console.log(this.tasks);
+    add(element) {
+        this.array.push(element);
     }
+}
+
+//Isso é uma herança
+class TaskList extends List{
+
 }
 
 const myList = new TaskList();
 document.getElementById('newTask').onclick = () => {
-    myList.addTask();
+    //Funciona do mesmo jeito
+    myList.add();
 }
