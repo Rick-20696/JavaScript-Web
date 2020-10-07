7315ea0a1934cb296b77e9e2115a177bde829a18
Ricardo <ricarosousa83@gmail.com>
2020-10-07 18:13:15 -0300

    Criando uma classe de tarefas

--- ES6 + a fundo/bundle.js
+++ ES6 + a fundo/bundle.js
@@ -6,20 +6,45 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-alert(""); //Códigos antes do ES6 será convertido para 
+//Códigos antes do ES6 será convertido para 
+var List = /*#__PURE__*/function () {
+  function List() {
+    _classCallCheck(this, List);
 
-var Test = /*#__PURE__*/function () {
-  function Test() {
-    _classCallCheck(this, Test);
+    this.datas = [];
   }
 
-  _createClass(Test, [{
-    key: "method",
-    value: function method() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
+  _createClass(List, [{
+    key: "add",
+    value: function add(data) {
+      this.datas.push(data);
+    }
   }]);
 
-  return Test;
+  return List;
 }();
+
+var TaskList = /*#__PURE__*/function () {
+  function TaskList() {
+    _classCallCheck(this, TaskList);
+
+    this.tasks = [];
+  } //Como meu contexto é de uma classe, eu preciso do this para acessar os atributos
+
+
+  _createClass(TaskList, [{
+    key: "addTask",
+    value: function addTask() {
+      this.tasks.push('New task');
+      console.log(this.tasks);
+    }
+  }]);
+
+  return TaskList;
+}();
+
+var myList = new TaskList();
+
+document.getElementById('newTask').onclick = function () {
+  myList.addTask();
+};
--- ES6 + a fundo/main.js
+++ ES6 + a fundo/main.js
@@ -1,12 +1,29 @@
-alert("");
-
 //Códigos antes do ES6 será convertido para 
-class Test {
-    method() {
-
+class List {
+    constructor() {
+        this.datas = [];
     }
 
-    outro() {
-        
+    add(data) {
+        this.datas.push(data);
     }
+}
+
+
+class TaskList {
+    
+    constructor(){
+        this.tasks = [];
+    }
+
+    //Como meu contexto é de uma classe, eu preciso do this para acessar os atributos
+    addTask() {
+        this.tasks.push('New task'); 
+        console.log(this.tasks);
+    }
+}
+
+const myList = new TaskList();
+document.getElementById('newTask').onclick = () => {
+    myList.addTask();
 }
