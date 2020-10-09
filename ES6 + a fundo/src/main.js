7ccc5ed6bc06128f79e0421b7bfed7ed92d9e7a3
Ricardo <ricarosousa83@gmail.com>
2020-10-09 18:32:33 -0300

    Começando com a mecânica do main.js

--- ES6 + a fundo/src/main.js
+++ ES6 + a fundo/src/main.js
@@ -1,13 +1,29 @@
-//Trabalhando com axios
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
 
-class api {
-    static async getUserInfo(user){
-        
-        //Todas as funções do axios retornam promisses
-        console.log(await axios.get(`https://api.github.com/users/${user}`))
-        
+        this.form = document.getElementById('form');
+
+        this.registerEvents();
+    }
+
+    registerEvents() {
+        this.form.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        //Essa linha previne que o formulário fique atualizando a página desnecessaramente
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'RicardoSousa83',
+            description: 'Estudante de Desenvolvimento de Sistemas',
+            avatar_ulr: 'https://avatars3.githubusercontent.com/u/62113652?=v4',
+            url: 'https://github.com/Rick-20696/Web',
+        });
+
+        console.log(this.repositories);
     }
 }
 
-api.getUserInfo('Rick-20696')
+new App();
