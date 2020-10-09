45169e42a0b87ae7261e930d6d5dccd51a5eec3f
Ricardo <ricarosousa83@gmail.com>
2020-10-09 08:43:50 -0300

    Revendo Axios e utilizando async await com a API do github

--- ES6 + a fundo/src/main.js
+++ ES6 + a fundo/src/main.js
@@ -1,16 +1,13 @@
-//Criando uma promise manualmente
-const promise = () => new Promise((resolve, reject) => {
-    setTimeout(() => resolve('ok'), 2000)
-});
+//Trabalhando com axios
+import axios from 'axios';
 
-//promise().then(response => {console.log(response)}).catch(error => {console.log(error)});
-
-//Fazendo com async await. É quase a mesma coisa que o .then. Porém mais fácil
-async function runPromise(){
-    //O await vai permitir que a linha de baixo execute apenas quando essa estiver pronta
-    const response = await promise();
-
-    console.log(response);
+class api {
+    static async getUserInfo(user){
+        
+        //Todas as funções do axios retornam promisses
+        console.log(await axios.get(`https://api.github.com/users/${user}`))
+        
+    }
 }
 
-runPromise();
+api.getUserInfo('Rick-20696')
