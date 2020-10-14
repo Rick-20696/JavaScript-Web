7b98cbcb2f402f4feace1002d488aa970fda6ef9
Ricardo <ricarosousa83@gmail.com>
2020-10-14 10:02:55 -0300

    Node-API

--- Node/Node-API/src/routes.js
+++ Node/Node-API/src/routes.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
-//Agora minha lógica fica no Controller. Aqui eu só chamo o nome da função que está sendo exportada!
 
+//Estou separando a instância de rotas do express e colocando na constante
+const routes = express.Router();
+
+//Agora minha lógica fica no Controller. Aqui eu só chamo o nome da função que está sendo exportada!
+//O parâmetro vem pela rota e é representado por "model/:nameParameter"
 routes.get('/products',  ProductController.index);
 routes.get('/products/:id', ProductController.show);
 
 routes.post('/products', ProductController.store);
 routes.put('/products/:id', ProductController.update);
 
-//O parâmetro vem pela rota e é representado por "model/:nameParameter"
 routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
