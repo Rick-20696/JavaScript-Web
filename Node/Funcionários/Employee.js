b6b7d980b080cc502443310250c56a9bf6d13ed4
Ricardo <ricarosousa83@gmail.com>
2020-10-19 17:54:47 -0300

    Instalação do nodemon

--- Node/Funcionários/Employee.js
+++ Node/Funcionários/Employee.js
@@ -7,12 +7,12 @@ axios.get(url).then(response => {
     //Aqui eu estou pegando, numa lista de 500 funcionários, qual a mulher chinesa com menor salário
     const data = response.data;
 
-    const getWoman = emp => emp.genero === 'M';
+    const getWoman = emp => emp.genero === 'F';
     const getNation = emp => emp.pais === 'China'
     const getMinSalary = (ac, s) => ac.salario < s.salario? ac : s
 
     //O fato de eu estár filtrando dos 500 apenas os chinese me trás um benefício de desempenho porque a lista será bem menor
     const employee = data.filter(getWoman).filter(getNation).reduce(getMinSalary);
     
-    console.log(data.filter(employee));
+    console.log(employee);
 })
