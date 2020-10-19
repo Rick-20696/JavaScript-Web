0e6d06c1a4168e3cf3e64682b2396299e6b30b62
Ricardo <ricarosousa83@gmail.com>
2020-10-18 23:49:13 -0300

    Aplicando conceitos de array num filtro realísitco

---
+++ Node/Funcionários/Employee.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
+
+//Fazendo uma requisição para o arquivo .json da url
+axios.get(url).then(response => {
+    
+    //Aqui eu estou pegando, numa lista de 500 funcionários, qual a mulher chinesa com menor salário
+    const data = response.data;
+
+    const getWoman = emp => emp.genero === 'M';
+    const getNation = emp => emp.pais === 'China'
+    const getMinSalary = (ac, s) => ac.salario < s.salario? ac : s
+
+    //O fato de eu estár filtrando dos 500 apenas os chinese me trás um benefício de desempenho porque a lista será bem menor
+    const employee = data.filter(getWoman).filter(getNation).reduce(getMinSalary);
+    
+    console.log(data.filter(employee));
+})
