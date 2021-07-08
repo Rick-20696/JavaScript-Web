f5c0abb0621cc72fcd19d6e68d6c5a070234c7c8
ricardo <ricarosousa83@gmail.com>
2021-07-08 13:33:02 -0300

    Carregando .css no javascript com html

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -1,4 +1,5 @@
 import People from "./people"
+import './assets/app.css'
 
 const p1 = new People()
 console.log(p1.greeting())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,5 +2,20 @@ const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
-  entry: './src/main.js' 
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/public'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/,
+        use: [
+          'style-loader', //Injeta o css via DOM
+          'css-loader' //interpret @import, url(), etc
+        ]
+      }
+    ]
+  }
 }
