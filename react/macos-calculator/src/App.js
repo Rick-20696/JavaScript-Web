efd47d19d1dd95d26ae8b303fc8d09267c4321cc
ricardo <ricarosousa83@gmail.com>
2021-07-12 05:37:49 -0300

    Criando projeto calculadora do Mac Os

---
+++ react/macos-calculator/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>A clone from Mac OS calculator</h1>
+      </header>
+      <footer>
+        <h2>Powered with React | by Sousa83</h2>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
