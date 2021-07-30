558007426b15a3e5336f7fffe8504b8c75762753
ricardo <ricarosousa83@gmail.com>
2021-07-29 21:57:08 -0300

    definindo estilo inicial e ajustando sistema grid

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,7 +1,8 @@
+import './App.css'
 import React from "react"
-import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
+import Logo from '../components/template/Logo'
 import Footer from '../components/template/Footer'
 
 export default props => 
